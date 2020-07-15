64ef3ea2c878a29b5c7322949399d71656e1835e
talvarez0507 <51244815+talvarez0507@users.noreply.github.com>
2020-07-15 11:20:15 -0400

    rename file to ServletHelper and move methods

--- portfolio/src/main/java/com/google/sps/data/ServletHelper.java
+++ portfolio/src/main/java/com/google/sps/data/ServletHelper.java
@@ -1,28 +1,19 @@
 package com.google.sps.data;
 
 import com.google.gson.Gson;
-import com.google.sps.data.*;
 import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+/** This class includes the helper methods used by ScheduleServlet.java */
+public class ServletHelper {
 
-/**
- * This class includes the utils used by ScheduleServlet.java 
- */
-public class ServletUtils {
-  
   // TODO(tomasalvarez): Add tests for this method.
   public static Collection<CalendarEvent> collectEventsFromJsonArray(JSONArray eventsArray) {
     Collection<CalendarEvent> events = new ArrayList<CalendarEvent>();
@@ -58,35 +49,10 @@ public class ServletUtils {
     return tasks;
   }
 
-  public static Optional<SchedulingAlgorithmType> getSchedulingAlgorithmTypeOptional(
-      String algorithmTypeString) {
-    // Here we should add a case for each new algorithm that is implemented.
-    switch (algorithmTypeString) {
-      case "SHORTEST_TASK_FIRST":
-        return Optional.of(SchedulingAlgorithmType.SHORTEST_TASK_FIRST);
-    }
-    return Optional.empty();
-  }
-
-  public static Optional<TaskScheduler> getTaskSchedulerOptional(
-      Optional<SchedulingAlgorithmType> schedulingAlgorithmTypeOptional) {
-    // This will always be present because in the doPost we return the method
-    // before the code gets to call this method if this Optional is not
-    // present.
-    SchedulingAlgorithmType schedulingAlgorithmType = schedulingAlgorithmTypeOptional.get();
-    // Here we should add a case for each new algorithm that is implemented.
-    switch (schedulingAlgorithmType) {
-      case SHORTEST_TASK_FIRST:
-        return Optional.of(new ShortestTaskFirstScheduler());
-    }
-    return Optional.empty();
-  }
-
   public static void returnEmptyArrayResponse(HttpServletResponse response) throws IOException {
     Gson gson = new Gson();
     String resultJson = gson.toJson(Arrays.asList());
     response.setContentType("application/json");
     response.getWriter().println(resultJson);
   }
-
 }
